da748e14d6bf7fbca3d4c47c5367f381c9f5a385
Olena <oh-i-lenka@gavatyuk.org.ua>
2016-11-15 13:30:40 +0100

    changed the class Bird: optimized method sing again

--- src/Hw_3_1/Bird.java
+++ src/Hw_3_1/Bird.java
@@ -3,13 +3,13 @@ package Hw_3_1;
 public class Bird {
 
     public static void main(String[] args) {
-        sing("I am walking");
-        sing("I am flying");
-        sing("I am singing");
-        sing("I am a Bird");
+        sing("walking");
+        sing("flying");
+        sing("singing");
+        sing("a Bird");
     }
 
     public static void sing(String song){
-       System.out.println((char)149 + song);
+       System.out.println((char)149 + "I am " + song);
     }
 }
