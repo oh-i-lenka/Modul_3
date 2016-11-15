4abb36e9f964007b477d91574e9e9efc6d215903
Olena <oh-i-lenka@gavatyuk.org.ua>
2016-11-15 10:10:54 +0100

    Class Bird has changed: added argument to the method sing()

--- src/Hw_3_1/Bird.java
+++ src/Hw_3_1/Bird.java
@@ -3,11 +3,11 @@ package Hw_3_1;
 public class Bird {
 
     public static void main(String[] args) {
-        sing();
+        char mark = (char)149;
+        sing(mark + "I am walking\n" + mark + "I am flying\n" + mark +"I am singing\n" + mark + "I am a Bird");
     }
 
-    public static void sing(){
-        char mark = (char)149;
-        System.out.println(mark + "I am walking\n" + mark + "I am flying\n" + mark +"I am singing\n" + mark + "I am a Bird");
+    public static void sing(String song){
+        System.out.println(song);
     }
 }
