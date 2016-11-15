a7ce75fbe1fc5c53afb16cfd709be7963469801a
Olena <oh-i-lenka@gavatyuk.org.ua>
2016-11-15 13:25:32 +0100

    changed the class Bird: changed method sing again, it contains only one argue, it invoked 4 times  in mainMethod

--- src/Hw_3_1/Bird.java
+++ src/Hw_3_1/Bird.java
@@ -3,16 +3,13 @@ package Hw_3_1;
 public class Bird {
 
     public static void main(String[] args) {
-        String line1, line2, line3, line4;
-        line1 = "I am walking";
-        line2 = "I am flying";
-        line3 = "I am singing";
-        line4 = "I am a Bird";
-        sing(line1, line2, line3, line4);
+        sing("I am walking");
+        sing("I am flying");
+        sing("I am singing");
+        sing("I am a Bird");
     }
 
-    public static void sing(String line1, String line2, String line3, String line4){
-        char mark = (char)149;
-        System.out.println(mark + line1 + "\n" + mark + line2 + "\n" + mark + line3 + "\n" + mark + line4);
+    public static void sing(String song){
+       System.out.println((char)149 + song);
     }
 }
